032d1d8e1474de4516641239005c14db0c148c89
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2019-11-15 17:59:25 -0300

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,33 +19,12 @@ class DishDetail extends React.Component {
 
     renderComments(comments) {
         if (comments != null) {
-            const convertMonth = (month) => {
-                if (month === '01') return "Jan";
-                if (month === '02') return "Feb";
-                if (month === '03') return "Mar";
-                if (month === '04') return "Apr";
-                if (month === '05') return "May";
-                if (month === '06') return "Jun";
-                if (month === '07') return "Jul";
-                if (month === '08') return "Aug";
-                if (month === '09') return "Sep";
-                if (month === '10') return "Oct";
-                if (month === '11') return "Nov";
-                if (month === '12') return "Dec";
-            }
             const values = comments.map((each) => {
-                let year = each.date.slice(0, 4);
-                let month = convertMonth(each.date.slice(5, 7));
-                let day = parseInt(each.date.slice(8, 10)) + 1;
-                if (day < 10) {
-                    day.toString();
-                    day = "0" + day;
-                }
                 return (
                     <div key={each.id} className="mt-1">
                         <ul className="list-unstyled">
                             <li>{each.comment}</li>
-                            <li>-- {each.author}, {month} {day}, {year}</li>
+                            <li>-- {each.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(each.date)))}</li>
                         </ul>
                     </div>
                 );
@@ -68,6 +47,7 @@ class DishDetail extends React.Component {
     render() {
         if (this.props.dish != null) {
             return (
+                <div className="container">
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             {this.renderDish(this.props.dish)}
@@ -76,6 +56,7 @@ class DishDetail extends React.Component {
                             {this.renderComments(this.props.dish.comments)}
                         </div>
                     </div>
+                </div>
             );
         }
         else {
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,38 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
-import DishDetail from "./DishdetailComponent"
 
 class Menu extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dish) {
-        this.setState({selectedDish: dish});
-    }
-
-    renderDish(dish) {
-        if (dish != null) {
-            return(
-               <DishDetail dish={this.state.selectedDish}/>
-            );
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
     }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -46,9 +25,6 @@ class Menu extends React.Component {
             <div className="container">
                 <div className="row">
                     {menu}
-                </div>
-                <div>
-                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
